d1762af6032cba1a2721d421668cc92de962837d
f4lco0n <pawel_sokol@o2.pl>
2020-03-19 11:39:08 +0100

    func test checks possibility to add element to list

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 
@@ -16,8 +17,26 @@ class NewVisitorTest(unittest.TestCase):
         """#start point of app"""
         self.browser.get('http://localhost:8000')
         self.assertIn('Listy',self.browser.title)
-        self.fail('Zakonczenie testu')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('Listy',header_text)
 
+        #find element by given id and test his attribute
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'),'Wpisz rzeczy do zrobienia')
+
+        #hardcode given text
+        inputbox.send_keys('Kupić pióra')
+        inputbox.send_keys(Keys.ENTER)
+
+        #find table by id and get rows
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+
+        #compare text from inputbox to updated table
+        self.assertTrue(
+            any(row.text == '1: Kupić pióra' for row in rows))
+
+        self.fail('Zakonczenie testu')
 
 if __name__=='__main__':
     unittest.main(warnings='ignore')
