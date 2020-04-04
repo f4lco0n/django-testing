5fbb05bd2531c21fd236aeb0927d173ca8b3c41c
f4lco0n <pawel_sokol@o2.pl>
2020-04-04 21:11:17 +0200

    add LiveServer to functional tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,11 @@
 import time
 from selenium import webdriver
+from django.test import LiveServerTestCase
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -23,7 +24,7 @@ class NewVisitorTest(unittest.TestCase):
 
     def test_can_start_list_and_retrieve_it_later(self):
         """#start point of app"""
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         self.assertIn('Listy',self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('Listy',header_text)
@@ -44,10 +45,8 @@ class NewVisitorTest(unittest.TestCase):
 
         self.check_for_row_in_list_table('2: Uzyc pior')
 
-        # self.fail('Zakonczenie testu')
+        self.fail('Zakonczenie testu')
 
-if __name__=='__main__':
-    unittest.main(warnings='ignore')
 
 
 
