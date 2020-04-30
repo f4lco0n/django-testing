7f24efac9853cd780cff0edbf6f50daec9dc12b4
f4lco0n <pawel_sokol@o2.pl>
2020-04-30 21:07:27 +0200

    Add URL and view to add new element to existing list

--- lists/tests.py
+++ lists/tests.py
@@ -58,8 +58,6 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response,'list.html')
 
 
-
-
     def test_displays_only_items_for_that_list(self):
         correct_list = List.objects.create()
         Item.objects.create(text='item1',list=correct_list)
@@ -75,6 +73,13 @@ class ListViewTest(TestCase):
         self.assertContains(response, 'item2')
         self.assertNotContains(response, 'item 1 for other_list')
         self.assertNotContains(response, 'item 2 for other_list')
+
+
+    def test_passes_correct_list_to_template(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+        response = self.client.get('/lists/%d/' % (correct_list.id,))
+        self.assertEqual(response.context['list'],correct_list)
 
 class NewListTest(TestCase):
 
@@ -94,4 +99,33 @@ class NewListTest(TestCase):
         )
         new_list = List.objects.first()
         self.assertRedirects(response,'/lists/%d/' % (new_list.id,))
+
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'Nowy element dla istniejacej listy'}
+        )
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text,'Nowy element dla istniejacej listy')
+        self.assertEqual(new_item.list,correct_list)
+
+    def test_redirects_to_list_view(self):
+
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            '/lists/%d/add_item' % (correct_list.id,),
+            data={'item_text': 'Nowy element dla istniejacej listy'}
+        )
+
+        self.assertRedirects(response,'/lists/%d/' % (correct_list.id,))
 
